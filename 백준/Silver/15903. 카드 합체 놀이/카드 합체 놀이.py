810d85ef300e59ed9c14b3b31cb7983c53269aa3
Eunseo-ee <155134542+Eunseo-ee@users.noreply.github.com>
2025-02-01 21:53:53 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 68 ms, Memory: 35600 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -0,0 +1,17 @@
+import sys
+from collections import deque
+import heapq
+
+input = sys.stdin.readline
+
+n,m=map(int,input().split())
+heap=list(map(int,input().split()))
+heapq.heapify(heap)
+
+for i in range(m):
+    a=heapq.heappop(heap)
+    b=heapq.heappop(heap)
+    heapq.heappush(heap,a+b)
+    heapq.heappush(heap, a + b)
+
+print(sum(heap))
