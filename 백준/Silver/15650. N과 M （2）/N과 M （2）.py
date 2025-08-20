9bbb9ef066ab1509b85a9e95987ef37293787e7a
Eunseo-ee <155134542+Eunseo-ee@users.noreply.github.com>
2025-08-21 01:26:06 +0900

    [Silver III] Title: N과 M (2), Time: 80 ms, Memory: 39408 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,18 +1,23 @@
-n,m=map(int,input().split())
+import queue
+import sys
+from collections import deque
 
-def dfs():
-    if len(num)==m and sorted(num)==num:
-        print(' '.join(map(str,num)))
+input=sys.stdin.readline
+
+##############################################################################################
+
+def dfs(start):
+    if len(s)==m:
+        print(' '.join(map(str,s)))
         return
-    for i in range(1,n+1):
-        if visited[i]==True:
-            continue
-        num.append(i)
-        visited[i]=True
-        dfs()
-        num.pop()
-        visited[i]=False
 
-num=[]
-visited=[False]*(n+1)
-dfs()
+    for i in range(start,n+1):
+        if i not in s:
+            s.append(i)
+            dfs(i+1)
+            s.pop()
+
+n,m=map(int,input().split())
+s=[]
+
+dfs(1)
