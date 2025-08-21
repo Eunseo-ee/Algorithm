5899f8fb7629a6944a72e795ccb396e7d25a1a16
Eunseo-ee <155134542+Eunseo-ee@users.noreply.github.com>
2025-08-22 01:16:12 +0900

    [Silver III] Title: N과 M (3), Time: 1172 ms, Memory: 39756 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,22 @@
+import queue
+import sys
+from collections import deque
+
+input=sys.stdin.readline
+
+##############################################################################################
+
+n,m=map(int,input().split())
+s=[]
+
+def dfs():
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+
+    for i in range(1,n+1):
+        s.append(i)
+        dfs()
+        s.pop()
+
+dfs()
