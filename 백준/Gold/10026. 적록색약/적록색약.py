25760c93c829c564e54a0c22c2c892797e6db804
Eunseo-ee <155134542+Eunseo-ee@users.noreply.github.com>
2026-04-06 14:21:10 +0900

    [Gold V] Title: 적록색약, Time: 76 ms, Memory: 36464 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,46 +1,53 @@
+import sys
+input = sys.stdin.readline
 from collections import deque
+import heapq
 
-def BFS(x,y):
-    q.append((x,y))
-    dx = [-1,0,1,0]
-    dy = [0,1,0,-1]
-    visited[x][y] = 1
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+yes=[]
+no=[]
+yes_cnt=0
+no_cnt=0
+
+dx=[0, 0, -1, 1]
+dy=[1, -1, 0, 0]
+
+def bfs(start_x, start_y, color, graph):
+    q=deque([(start_x, start_y)])
+    graph[start_y][start_x]=0
+
     while q:
-        x, y = q.popleft()
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
-            # 인덱스 범위 안에 있으면서, 같은 색이면서, 방문 안한 경우
-            if 0<=nx<N and 0<=ny<N and a[nx][ny] == a[x][y] and not visited[nx][ny]:
-                visited[nx][ny] = 1  # 방문체크 후 큐에 넣음
-                q.append((nx,ny))
+        x,y=q.popleft()
 
+        for i in range(4):
+            nx,ny=x+dx[i], y+dy[i]
 
-N = int(input())
-a = [list(input()) for _ in range(N)]
-q = deque()
+            if 0<=nx<n and 0<=ny<n:
+                if graph[ny][nx]==color:
+                    q.append((nx, ny))
+                    graph[ny][nx]=0
 
-# 적록색약 아닌 경우
-visited = [[0] * N for _ in range(N)]
-cnt1 = 0
-for i in range(N):
-    for j in range(N):
-        if not visited[i][j]:  # 아직 방문 안한 경우만 체킹
-            BFS(i,j)
-            cnt1 += 1
+for _ in range(n):
+    x=list(input().rstrip())
+    no.append(x)
+    tmp=[]
+    for i in range(len(x)):
+        if x[i]=='G':
+            tmp.append('R')
+        else:
+            tmp.append(x[i])
+    yes.append(tmp)
 
-# 적록색약인 경우
-for i in range(N):
-    for j in range(N):
-        if a[i][j] == 'G':
-            a[i][j] = 'R'
+for i in range(n):
+    for j in range(n):
+        if no[i][j]!=0:
+            bfs(j,i,no[i][j],no)
+            no_cnt+=1
+        if yes[i][j]!=0:
+            bfs(j,i,yes[i][j],yes)
+            yes_cnt+=1
 
-visited = [[0] * N for _ in range(N)]
-cnt2 = 0
-for i in range(N):
-    for j in range(N):
-        if not visited[i][j]:
-            BFS(i,j)
-            cnt2 += 1
-
-print(cnt1, cnt2)
+print(no_cnt, yes_cnt)
